e1aa74ae24520cca9e5e7ed0ff7d24a6dc4c1731
kkaarroollm <mkarol.4514@gmail.com>
2025-04-22 14:06:10 +0200

    notification-service: linting

--- notifications/src/config/settings.py
+++ notifications/src/config/settings.py
@@ -8,7 +8,7 @@ class EnvironmentEnum(str, Enum):
     PRODUCTION = "PRODUCTION"
     DEVELOPMENT = "DEVELOPMENT"
 
-    def docs_available(self):
+    def docs_available(self) -> bool:
         show_docs_environments = {EnvironmentEnum.DEVELOPMENT}
         return self in show_docs_environments
 
@@ -24,7 +24,7 @@ class Settings(BaseSettings):
 
     redis_host: str = "localhost"
     redis_port: int = 6379
-    redis_url: str = f"redis://localhost:6379"
+    redis_url: str = ""
 
     orders_stream: str = "orders-stream"
     deliveries_stream: str = "deliveries-stream"
@@ -35,16 +35,15 @@ class Settings(BaseSettings):
     cors_allow_origins: list[str] = []
     cors_allow_methods: list[str] = []
     cors_allow_headers: list[str] = []
-    allowed_hosts: list[str] = []
 
     @model_validator(mode="after")
-    def setup_dynamic(self):
+    def setup_dynamic(self) -> "Settings":
         if self.debug:
             self.cors_allow_origins = ["*"]
             self.cors_allow_methods = ["*"]
             self.cors_allow_headers = ["*"]
-            self.allowed_hosts = ["*"]
-        self.redis_url = f"redis://{self.redis_host}:{self.redis_port}"
+        if not self.redis_url:
+            self.redis_url = f"redis://{self.redis_host}:{self.redis_port}"
         return self
 
     model_config = SettingsConfigDict(env_file=".env")
--- notifications/src/main.py
+++ notifications/src/main.py
@@ -2,25 +2,14 @@ import logging
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request, Response
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
-from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
-from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
-from src.config import EnvironmentEnum, settings
+from src.config import settings
 from src.lifespan import startup, teardown
 from src.routes import router
 from src.websockets import ws_order_status_manager
-
-app_config = {
-    "title": settings.title,
-    "version": settings.version,
-    "contact": {"name": settings.contact_name, "email": settings.contact_email},
-}
-
-if not settings.environment.docs_available():
-    app_config["openapi_url"] = None
 
 
 @asynccontextmanager
@@ -31,10 +20,19 @@ async def lifespan(app_: FastAPI) -> AsyncGenerator:
     await teardown(app_)
 
 
-app = FastAPI(**app_config, lifespan=lifespan)
+app: FastAPI = FastAPI(
+    title=settings.title,
+    version=settings.version,
+    contact={
+        "name": settings.contact_name,
+        "email": settings.contact_email,
+    },
+    openapi_url=None if not settings.environment.docs_available() else "/openapi.json",
+    lifespan=lifespan,
+)
 
 
-@app.websocket("/order-tracking/{order_id}/ws")
+@app.websocket("/ws/v1/order-tracking/{order_id}")
 async def websocket_order_tracking(websocket: WebSocket, order_id: str) -> None:
     await ws_order_status_manager.connect(order_id, websocket)
     notifications_repo = app.state.notification_repository
@@ -57,12 +55,6 @@ async def websocket_order_tracking(websocket: WebSocket, order_id: str) -> None:
 
 app.include_router(router)
 
-
-if settings.environment == EnvironmentEnum.PRODUCTION:
-    app.add_middleware(HTTPSRedirectMiddleware)
-    app.add_middleware(TrustedHostMiddleware, allowed_hosts=settings.allowed_hosts)
-
-
 app.add_middleware(GZipMiddleware)
 app.add_middleware(
     CORSMiddleware,
@@ -71,9 +63,3 @@ app.add_middleware(
     allow_methods=settings.cors_allow_methods,
     allow_headers=settings.cors_allow_headers,
 )
-
-@app.middleware("http")
-async def add_csp_header(request: Request, call_next):
-    response: Response = await call_next(request)
-    response.headers["Content-Security-Policy"] = "upgrade-insecure-requests"
-    return response
